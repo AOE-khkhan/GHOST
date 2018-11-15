14a753c9db46b3129a2ce9dc4adf010b07a4c6fd
Joshua Christian <joshua3christianx@gmail.com>
2018-11-15 17:17:38 +0100

    wave method: working on assigning weights to influencing states

--- processor.py
+++ processor.py
@@ -10,108 +10,103 @@ Project: GHOST
 import re
 from itertools import combinations
 
+# import from my framework
+from functions import log
+
 class Processor:
     """docstring for Processor"""    
     
-    def __init__(self):
+    def __init__(self, n_sensors=8, size=3):
+        # if to show output
         self.log_state = True
-        self.SIZE = 3
 
-        # holds the dendrites combinations that exist and indexes them
-        self.dendrites2index = {}
-        self.index2dendrites = {}
+        # processor size in bits
+        self.SIZE = size
 
-        # holds the connection between dendrites
-        # and possibly higher order connections (this should represent deep concepts) 
-        self.connections2index = [{} for _ in range(self.SIZE)]
-        self.index2connections = [{} for _ in range(self.SIZE)]
+        # sensory binary data size
+        self.STATE_SIZE = 2**n_sensors
 
-        # holds the indices of the last dendrites
-        self.last_dendrites = None
+        # holds the addreses of the sensory state
+        self.register = [{} for _ in range(self.SIZE)]  #binary_data:index
+        self.registry = [{} for _ in range(self.SIZE)]  #index:binary_data
 
-    def connect(self, d1, d2, level):
-        length = len(self.connections2index[level])
-        connections = []
-        for dx in d1:
-            for dy in d2:
-                connection = tuple(sorted([dx,dy]))
-                print(connection)
-                if connection in self.connections2index[level]:
-                    connection_index = self.connections2index[level][connection]
+        # matrix of nodes that process and project data
+        self.nodes = [[[0 for _ in range(self.STATE_SIZE)] for __ in range(self.STATE_SIZE)] for ___ in range(self.SIZE)]
 
-                else:
-                    self.connections2index[level][connection] = length
-                    connection_index = length
-                    length += 1
+        # holds the last information of size length
+        self.context = [[] for _ in range(self.SIZE)]
 
-                connections.append(connection_index)
-        return connections
+        # contains the output wave function
+        self.processes = [[[0 for _ in range(self.STATE_SIZE)] for __ in range(self.STATE_SIZE)] for ___ in range(self.SIZE)]
+
+    def addToContext(self, data, level):
+        if len(self.context[level]) == self.SIZE:
+            self.context[level].pop()
+        
+        self.context[level].insert(0, bdi)
+        return
 
     def log(self, output, title=None):
         if not self.log_state:
             return
 
-        if title != None:
-            print('\n {} \n{}'.format(title, ''.join(['=' for _ in range(len(title)+2)])))
-
-        print('{}\n'.format(output))
+        log(output, title)
         return
 
-    def getDendrites(self, input_list):
-        return [i for i, x in enumerate(input_list) if x == '1']
+    def normalize(self, li, factor=None):
+        s = sum(li)
 
-    def getHighOrderDendrites(self, dendrites):
-        '''
-        this combines dendrites to see which combination influences next process
-        '''
-        length = len(dendrites)
-        new_dendrites = []
-        for r in range(2, length+1):
-            combs = combinations(dendrites, r)
+        if s == 0:
+            return [0 for _ in li]
 
-            for comb in combs:
-                new_dendrites.append(comb)
+        else:
+            return [x/s for x in li]
 
-        new_dendrites.extend(dendrites)
-        return new_dendrites
+    def process(self, binary_data):
+        # self.log(binary_data, 'bin_data')
 
-    def process(self, binary_str):
-        input_list = list(binary_str)
+        # save the sensoy state and get the index
+        binary_data_index = self.save(binary_data, 0)
 
-        # get the dendrites formed
-        dendrites = self.getDendrites(input_list)
-        # self.log(dendrites, 'dendrites')
+        # self.log(binary_data_index, 'bin_data_index')
 
-        # get high order dendrites
-        dendrites = self.getHighOrderDendrites(dendrites)
+        # add data to context
+        self.addToContext(binary_data_index)
+
+        # reinforce the nodes to make connections in wave forms ( of past data and current one)
+        for i, bdi in enumerate(self.context):
+            self.nodes[i][bdi][binary_data_index] += 1
         
-        # logs the dendrites in memory and represent them as indexes
-        dendrites = self.setupDendrites(dendrites)
+        # self.log(self.nodes, 'nodes')
+        # add the influence of the current data to process
+        for i in range(self.SIZE):
+            weights = self.normalize(self.nodes[i][binary_data_index])
+            for j in range(self.STATE_SIZE):
+                self.processes[i][binary_data_index][j] += weights[j]
 
-        self.log(dendrites, 'dendrites updated')
+        predicted_outputs = [x/self.SIZE for x in self.processes[0][binary_data_index]]
+        # self.log(self.processes[0][binary_data_index], 'processes')
+        # self.log(predicted_outputs, 'predicted_outputs')
+        
+        m = max(predicted_outputs)
+        predicted_outputs = [self.registry[i] for i, x in enumerate(predicted_outputs) if m == x and m > 0]
+        # predicted_output = predicted_outputs[0] if len(predicted_outputs) > 0 else None
 
-        if self.last_dendrites != None:
-            connections = self.connect(dendrites, self.last_dendrites, 1)
-            self.log(connections, 'connections')
+        # self.log(m, 'max')
+        # self.log(predicted_output, 'output')
 
+        # restructure the processes
+        self.processes.append([[0 for _ in range(self.STATE_SIZE)] for __ in range(self.STATE_SIZE)])
+        self.processes = self.processes[1:]
 
-        self.last_dendrites = dendrites
-        return
+        return predicted_outputs, m
 
+    def save(self, data, level):
+        if data in self.register[level]:
+            return self.register[level][data]
 
-    def setupDendrites(self, dendrites):
-        length = len(self.dendrites2index)
-        dendrite_indices = []
-        for dendrite in dendrites:
-            if dendrite not in self.dendrites2index:
-                self.dendrites2index[dendrite] = length
-                self.index2dendrites[length] = dendrite
-                index = length
-                length += 1
-
-
-            else:
-                index = self.dendrites2index[dendrite]
-
-            dendrite_indices.append(index)
-        return dendrite_indices
+        else:
+            length = len(self.register[level])
+            self.register[level][data] = length
+            self.registry[level][length] = data
+            return length
