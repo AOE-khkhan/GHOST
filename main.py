2cfd01eda3a3e26b5a153ca06735bbc470f3b969
Joshua Christian <joshua3christianx@gmail.com>
2018-11-15 17:14:19 +0100

    wave method: working on assigning weights to influencing states

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ Project: GHOST
 
 # import lib code
 from processor import Processor
-from functions import log
 
 # initialize objects
 PROCESSOR = Processor()
@@ -20,46 +19,21 @@ def toChar(b):
     n = int('0b{}'.format(b), 2)
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
 
-def train(filepath):
-    with open(filepath, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip()
-
-            if line == '':
-                continue
-
-            for s in list(line):
-                yield s
-            yield '`'
-
 def main():
     # start the sensor
-    input_data = '''hello`hi`i live in nigeria, it is a very big place`hello`hi`what is 1+4?`hello`hi`my name is jack`hi`i am james, i come from the north.`do you speak latin?`no`good morning`hi`how are you doing?`i am great thank you`hello, my name is uri`hi, i'm emma.`hey`hey`can you come today?`no`see me tommorow`hi janet`hello kareem`hello`'''
+    # input_data = '''hello`hi`i live in nigeria, it is a very big place`hello`hi`what is 1+4?`hello`hi`my name is jack`hi`i am james, i come from the north.`do you speak latin?`no`good morning`hi`how are you doing?`i am great thank you`hello, my name is uri`hi, i'm emma.`hey`hey`can you come today?`no`see me tommorow`hi janet`hello kareem`hello`'''
 
-    # input_data = "ho`hi`ho`hi`"
-    # input_data = "hi`hello`two`hi`"
+    # input_data = "hello`hi`hello`hi`"
+    input_data = "hi`"
     # input_data = 'hi`hello`1+1 is 2`1+4 is 5`2+3 is 5`5+2 is 7`3+1 is 4`3+4 is 7`2+1 is 3`4+4 is 8`what is 2+3?`5`what is 2+1?`4`what is 3+4?`7`what is 5+2?`7`what is 1+1?`2`hi`'
-    # input_data = train('train.txt')
-    last_output_data = None
+    
     for data in input_data:
         data_binary_str = toBin(data)
-        
-        # log(data, 'input_data')
-        # log(data_binary_str, 'input_data_binary')
+        print(data, data_binary_str)
 
-        output_binary_strs, weight = PROCESSOR.process(data_binary_str)
-        od = []
-        output_data = None    
-        for output_binary_str in output_binary_strs:
-            output_data = toChar(output_binary_str)
+        output_binary_str = PROCESSOR.process(data_binary_str)
+        # print(output_binary_str, toChar(output_binary_str))
 
-            # log(output_binary_str, 'output_data_binary')
-            # log(output_data, 'output_data')
-            od.append(output_data)
-        last_output_data = ', '.join(od)
-        print('input_data = {}, predicted_output_data = {}, weight = {}'.format(data, last_output_data, weight))
 
 if __name__ == '__main__':
     main()
-    
--- processor.py
+++ processor.py
@@ -10,103 +10,108 @@ Project: GHOST
 import re
 from itertools import combinations
 
-# import from my framework
-from functions import log
-
 class Processor:
     """docstring for Processor"""    
     
-    def __init__(self, n_sensors=8, size=3):
-        # if to show output
+    def __init__(self):
         self.log_state = True
+        self.SIZE = 3
 
-        # processor size in bits
-        self.SIZE = size
+        # holds the dendrites combinations that exist and indexes them
+        self.dendrites2index = {}
+        self.index2dendrites = {}
 
-        # sensory binary data size
-        self.STATE_SIZE = 2**n_sensors
+        # holds the connection between dendrites
+        # and possibly higher order connections (this should represent deep concepts) 
+        self.connections2index = [{} for _ in range(self.SIZE)]
+        self.index2connections = [{} for _ in range(self.SIZE)]
 
-        # holds the addreses of the sensory state
-        self.register = [{} for _ in range(self.SIZE)]  #binary_data:index
-        self.registry = [{} for _ in range(self.SIZE)]  #index:binary_data
+        # holds the indices of the last dendrites
+        self.last_dendrites = None
 
-        # matrix of nodes that process and project data
-        self.nodes = [[[0 for _ in range(self.STATE_SIZE)] for __ in range(self.STATE_SIZE)] for ___ in range(self.SIZE)]
+    def connect(self, d1, d2, level):
+        length = len(self.connections2index[level])
+        connections = []
+        for dx in d1:
+            for dy in d2:
+                connection = tuple(sorted([dx,dy]))
+                print(connection)
+                if connection in self.connections2index[level]:
+                    connection_index = self.connections2index[level][connection]
 
-        # holds the last information of size length
-        self.context = [[] for _ in range(self.SIZE)]
+                else:
+                    self.connections2index[level][connection] = length
+                    connection_index = length
+                    length += 1
 
-        # contains the output wave function
-        self.processes = [[[0 for _ in range(self.STATE_SIZE)] for __ in range(self.STATE_SIZE)] for ___ in range(self.SIZE)]
-
-    def addToContext(self, data, level):
-        if len(self.context[level]) == self.SIZE:
-            self.context[level].pop()
-        
-        self.context[level].insert(0, bdi)
-        return
+                connections.append(connection_index)
+        return connections
 
     def log(self, output, title=None):
         if not self.log_state:
             return
 
-        log(output, title)
+        if title != None:
+            print('\n {} \n{}'.format(title, ''.join(['=' for _ in range(len(title)+2)])))
+
+        print('{}\n'.format(output))
         return
 
-    def normalize(self, li, factor=None):
-        s = sum(li)
+    def getDendrites(self, input_list):
+        return [i for i, x in enumerate(input_list) if x == '1']
 
-        if s == 0:
-            return [0 for _ in li]
+    def getHighOrderDendrites(self, dendrites):
+        '''
+        this combines dendrites to see which combination influences next process
+        '''
+        length = len(dendrites)
+        new_dendrites = []
+        for r in range(2, length+1):
+            combs = combinations(dendrites, r)
 
-        else:
-            return [x/s for x in li]
+            for comb in combs:
+                new_dendrites.append(comb)
 
-    def process(self, binary_data):
-        # self.log(binary_data, 'bin_data')
+        new_dendrites.extend(dendrites)
+        return new_dendrites
 
-        # save the sensoy state and get the index
-        binary_data_index = self.save(binary_data, 0)
+    def process(self, binary_str):
+        input_list = list(binary_str)
 
-        # self.log(binary_data_index, 'bin_data_index')
+        # get the dendrites formed
+        dendrites = self.getDendrites(input_list)
+        # self.log(dendrites, 'dendrites')
 
-        # add data to context
-        self.addToContext(binary_data_index)
-
-        # reinforce the nodes to make connections in wave forms ( of past data and current one)
-        for i, bdi in enumerate(self.context):
-            self.nodes[i][bdi][binary_data_index] += 1
+        # get high order dendrites
+        dendrites = self.getHighOrderDendrites(dendrites)
         
-        # self.log(self.nodes, 'nodes')
-        # add the influence of the current data to process
-        for i in range(self.SIZE):
-            weights = self.normalize(self.nodes[i][binary_data_index])
-            for j in range(self.STATE_SIZE):
-                self.processes[i][binary_data_index][j] += weights[j]
+        # logs the dendrites in memory and represent them as indexes
+        dendrites = self.setupDendrites(dendrites)
 
-        predicted_outputs = [x/self.SIZE for x in self.processes[0][binary_data_index]]
-        # self.log(self.processes[0][binary_data_index], 'processes')
-        # self.log(predicted_outputs, 'predicted_outputs')
-        
-        m = max(predicted_outputs)
-        predicted_outputs = [self.registry[i] for i, x in enumerate(predicted_outputs) if m == x and m > 0]
-        # predicted_output = predicted_outputs[0] if len(predicted_outputs) > 0 else None
+        self.log(dendrites, 'dendrites updated')
 
-        # self.log(m, 'max')
-        # self.log(predicted_output, 'output')
+        if self.last_dendrites != None:
+            connections = self.connect(dendrites, self.last_dendrites, 1)
+            self.log(connections, 'connections')
 
-        # restructure the processes
-        self.processes.append([[0 for _ in range(self.STATE_SIZE)] for __ in range(self.STATE_SIZE)])
-        self.processes = self.processes[1:]
 
-        return predicted_outputs, m
+        self.last_dendrites = dendrites
+        return
 
-    def save(self, data, level):
-        if data in self.register[level]:
-            return self.register[level][data]
 
-        else:
-            length = len(self.register[level])
-            self.register[level][data] = length
-            self.registry[level][length] = data
-            return length
+    def setupDendrites(self, dendrites):
+        length = len(self.dendrites2index)
+        dendrite_indices = []
+        for dendrite in dendrites:
+            if dendrite not in self.dendrites2index:
+                self.dendrites2index[dendrite] = length
+                self.index2dendrites[length] = dendrite
+                index = length
+                length += 1
+
+
+            else:
+                index = self.dendrites2index[dendrite]
+
+            dendrite_indices.append(index)
+        return dendrite_indices
